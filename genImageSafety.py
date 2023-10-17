88ebe4ee513abb03963145098e4d7e109c65495e
aoaoaoa14 <ne211159@senshu-u.jp>
2023-10-17 14:09:01 +0900

    jpgに変換

--- genImageSafety.py
+++ genImageSafety.py
@@ -14,14 +14,14 @@ from azure.ai.contentsafety.models import AnalyzeImageOptions, ImageData
 global image_path
 development = 1
 
+
 def analyze_image():
     print("SafetyCheckStarted.")
-    endpoint = os.environ.get('CONTENT_SAFETY_ENDPOINT')
-    key = os.environ.get('CONTENT_SAFETY_KEY')
+    endpoint = os.environ.get("CONTENT_SAFETY_ENDPOINT")
+    key = os.environ.get("CONTENT_SAFETY_KEY")
 
     # Create an Content Safety client
     client = ContentSafetyClient(endpoint, AzureKeyCredential(key))
-
 
     # Build request
     with open(image_path, "rb") as file:
@@ -48,19 +48,20 @@ def analyze_image():
     if response.violence_result:
         print(f"Violence severity: {response.violence_result.severity}")
     upload_image()
-        
+
 
 def upload_image():
     # user_id = input("ユーザIDを入力してん")
-    url = "http://127.0.0.1:3000/users/" + user_id + "/updatePicture"
+    url = "http://localhost:3000/users/" + user_id + "/updatePicture"
 
-    files = {'picture': open(image_path, 'rb')}
-    response = requests.patch(url, auth=HTTPBasicAuth('user', 'pass'), files=files)
+    files = {"picture": open(image_path, "rb")}
+    response = requests.patch(url, auth=HTTPBasicAuth("user", "pass"), files=files)
 
     if response.status_code == 200:
-        print('Update successful')
+        print("Update successful")
     else:
-        print('Update failed')
+        print("Update failed")
+
 
 global filename
 os.chdir("C://Users//ODPJ2023//Documents//SD2_CUI//outputs")
@@ -70,17 +71,17 @@ url = "http://127.0.0.1:7860"
 
 # 時刻のイニシャライズ
 t_delta = datetime.timedelta(hours=9)
-JST = datetime.timezone(t_delta, 'JST')
+JST = datetime.timezone(t_delta, "JST")
 now = datetime.datetime.now(JST)
 
-key = b'Enter your key here'
-print("QRコードをかざしてください： ", end="")
+key = b"Enter your key here"
+print("QRコードをかざしてください：", end="")
 QR_input = input().strip()
 # 暗号鍵と初期化ベクトル（IV）
 iv = QR_input[:24]
-print (iv)
+print(iv)
 crypted = QR_input[24:]
-print (crypted)
+print(crypted)
 
 # KEYからダイジェスト計算
 key_dg = hashlib.sha256(key).digest()
@@ -95,12 +96,12 @@ crypted = base64.b64decode(crypted)
 prompt = cipher.decrypt(crypted)
 
 # パディングを取り除く（もしあれば）
-prompt = prompt.rstrip(b'\0')
+prompt = prompt.rstrip(b"\0")
 
 # バイト列を文字列に変換
-prompt = prompt.decode('utf-8')
+prompt = prompt.decode("utf-8")
 global user_id
-user_id = prompt.split(',')[0][1:]
+user_id = prompt.split(",")[0][1:]
 print(user_id)
 
 payload = {
@@ -109,33 +110,31 @@ payload = {
     "batch_size": 1,
     "steps": 20,
     "cfg_scale": 7,
-    "width": 512,
-    "height": 512,
+    "width": 1000,
+    "height": 1000,
     "negative_prompt": "EasyNegative",
     "s_noise": 1,
     "script_args": [],
-"sampler_index": "Euler a",
+    "sampler_index": "Euler a",
 }
 print("生成中...")
-response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+response = requests.post(url=f"{url}/sdapi/v1/txt2img", json=payload)
 r = response.json()
 
 # pngに情報を書き込み
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+for i in r["images"]:
+    image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
 
-    png_payload = {
-        "image": "data:image/png;base64," + i
-    }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+    png_payload = {"image": "data:image/png;base64," + i}
+    response2 = requests.post(url=f"{url}/sdapi/v1/png-info", json=png_payload)
     pnginfo = PngImagePlugin.PngInfo()
     if development:
         pnginfo.add_text("parameters", response2.json().get("info"))
     else:
         pnginfo.add_text("Created by ", "小田切プロジェクト2023")
     # pnginfo.add_text("safetyInfo", safetyResult)
-    filename = str(now.strftime('%Y%m%d%H%M%S') + ".png")
-    image.save(filename, pnginfo=pnginfo)
+    filename = str(now.strftime("%Y%m%d%H%M%S") + ".jpg")
+    image.save(filename, "JPEG", quality=98, pnginfo=pnginfo)
     image_path = os.path.join(filename)
 
 print("Done!\n")
